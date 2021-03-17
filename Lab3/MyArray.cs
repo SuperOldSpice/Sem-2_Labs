603426fe3d25d32de08339a887973130043d42c6
SuperOldSpice <71700242+SuperOldSpice@users.noreply.github.com>
2021-03-17 23:25:20 +0200

    Add files via upload

---
+++ Lab3/MyArray.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ArrayLibrary
+{
+    public class MyArray
+    {
+        private string[] _strings; //strings array
+        private int _length;        //number of strings
+        private int _consonants_number;     //number of consonants
+        public MyArray(string[] str, int n)     //constructor
+        {
+            _length = n;
+            _strings = new string[_length];
+            for (int i = 0; i < _length; i++)
+            {
+                _strings[i] = str[i];
+            }
+        }
+
+        public void PrintStrings()      //array output
+        {
+            for (int i = 0; i < _length; i++)
+            {
+                Console.WriteLine(_strings[i]);
+            }
+        }
+
+        public string this[int index]      //property get string
+        {
+            get
+            {
+                return _strings[index];
+            }
+            set
+            {
+                _strings[index] = value;
+            }
+        }
+
+        public int GetConsonantsNumber      //property get consonants number
+        {
+            get
+            {
+                int counter = 0;
+
+                for(int i = 0; i < _length; i++)
+                {
+                    string str = _strings[i].ToLower();
+
+                    for (int j = 0; j < str.Length; j++)
+                    {
+                        string letter = str[j].ToString();
+
+                        if (!"aeouyi".Contains(letter) && letter != " ")
+                        {
+                            counter++;
+                        }
+                    }
+
+                }
+
+                _consonants_number = counter;
+
+                return _consonants_number;
+            }
+        }
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArrayLibrary;
+
+namespace Lab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number_of_strings; //number of strings
+            string[] strings = Input(out number_of_strings);  //strings array
+
+            MyArray myStrings = new MyArray(strings, number_of_strings); //new class object
+
+            Console.WriteLine("Input the number of string you wanna get:");
+            int string_number = Convert.ToInt32(Console.ReadLine());
+            string s = myStrings[string_number];
+            Console.WriteLine("Your string:");
+            Console.WriteLine(s);
+            Console.Write("\n");
+
+            int number_of_consonants = myStrings.GetConsonantsNumber;  // number of consonants
+
+            Console.WriteLine("Number of consonants in array: {0}", number_of_consonants); //output
+            Console.ReadKey();
+        }
+
+        static string[] Input(out int n)  //strings input
+        {
+            Console.WriteLine("Input number of strings:");
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] strings = new string[n];
+            Console.Write("\n");
+            Console.WriteLine("Input your strings:");
+            for (int i = 0; i < n; i++)
+            {
+                strings[i] = Console.ReadLine();
+            }
+            Console.Write("\n");
+            return strings;
+        }
+    }
+}
